cf9ae27f20288951b69b361281707841722141e7
TheAlecx <alexandre.leblanc.99@videotron.ca>
2026-02-18 10:03:55 -0500

    updat 7

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,8 +3,10 @@ dotenv.config();
 
 import app from "./app";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`API médicale lancée sur le port ${PORT}`);
 });
+
+export default app;
